16fe08cd6144ec97caa88e023177c684d6ab8115
Goemon <k2rmasakazu@gmail.com>
2019-04-12 13:58:43 +0900

    Add user image

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,18 @@
 class ApplicationController < ActionController::Base
 
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :configure_account_update_parameters, if: :devise_controller?
 
-  
 
+
+  # 新規登録時(sign_up時)にnameというキーのパラメーターを追加で許可する
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name]) # 新規登録時(sign_up時)にnameというキーのパラメーターを追加で許可する
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
+
+  # user edit時にname, imageというキーのパラメーターを追加で許可する
+  def configure_account_update_parameters
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :image])
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :name, presence: true, length: { maximum: 10}
+  mount_uploader :image, ImageUploader
   has_many :posts
 end
