c1a40beed0e665a0f6a05c06041c27fe26737df3
Goemon <k2rmasakazu@gmail.com>
2019-04-25 17:51:04 +0900

    update views and create home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
+  root to: 'static_pages#home'
   devise_for :users
-  root to: 'posts#index'
   resources :posts do
     post :confirm, action: :confirm_new, on: :new
     patch :confirm2, action: :confirm_edit, on: :member
