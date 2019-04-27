07d8bd849677d27cfdad304729fcb246d5adc8c7
Goemon <k2rmasakazu@gmail.com>
2019-04-27 19:23:37 +0900

    #3 add comment

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
+  get 'comments/create'
+  get 'comments/destroy'
   root to: 'static_pages#home'
   devise_for :users
   resources :posts do
     post :confirm, action: :confirm_new, on: :new
     patch :confirm2, action: :confirm_edit, on: :member
+    resources :comments, only: [:create, :destroy]
   end
   resources :users, :only => [:index, :show]
   resources :likes, only: [:create, :destroy]
