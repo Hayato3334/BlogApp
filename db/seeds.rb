ba14b3c21f2f188fa0fd1e904bdb393977c3f552
Goemon <k2rmasakazu@gmail.com>
2019-05-01 16:27:17 +0900

    deploy

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,10 @@
 Faker::Config.locale = :ja
 
-
+# ユーザー
+User.create!(name:  "Sample",
+             email: "sample@blogapp.org",
+             password:              "sample",
+             password_confirmation: "sample")
 
 30.times do |n|
   name  = Faker::Name.name
@@ -10,4 +14,20 @@ Faker::Config.locale = :ja
                email: email,
                password:              password,
                password_confirmation: password)
+end
+
+# ブログ
+users = User.order(:created_at).take(6)
+5.times do |n|
+  title = "筋トレ#{n+1}日目"
+  body = "ベンチブレス100kg * #{n+1}rep"
+  users.each { |user| user.posts.create!(title: title, body: body) }
+end
+
+# ライク
+users = User.all
+posts = Post.all
+5.times do |n|
+  m = n + 1
+  users[0..m].each { |liker| posts[n].likes.create!(user_id: liker.id) }
 end
